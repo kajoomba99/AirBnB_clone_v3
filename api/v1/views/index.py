861fa99cbc566caaf6f9abf93b5d9ab686ac7003
David Corredor <vidmore8@gmail.com>
2020-09-09 19:56:46 -0500

    [FIX] - Changing clasess in the index file

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,13 @@ Index file of views packages
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
+from models.amenity import Amenity
+from models.state import State
+from models.place import Place
+from models.city import City
+from models.review import Review
+from models.user import User
+from models import storage
 
 
 @app_views.route('/status', strict_slashes=False)
@@ -20,9 +27,9 @@ def stats():
     """
     Return the number of each object by type
     """
-    return jsonify({'amenities': storage.count('Amenity'),
-                    'cities': storage.count('City'),
-                    'places': storage.count('Place'),
-                    'reviews': storage.count('Review'),
-                    'states': storage.count('State'),
-                    'users': storage.count('User')})
+    return jsonify({'amenities': storage.count(Amenity),
+                    'cities': storage.count(City),
+                    'places': storage.count(Place),
+                    'reviews': storage.count(Review),
+                    'states': storage.count(State),
+                    'users': storage.count(User)})

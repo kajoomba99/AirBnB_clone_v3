ee3d3d0e6cc29168368e18915e0af709ccd1069a
laucavv <laucavv23@gmail.com>
2020-09-25 17:56:44 -0500

    task3

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,7 +10,7 @@ from os import getenv
 
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 app.register_blueprint(app_views)
 
